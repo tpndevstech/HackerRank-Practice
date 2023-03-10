719114bb8959c3400efba00a788a4faa45a1bb0e
Tapan Anand <70485285+tpndevstech@users.noreply.github.com>
2023-03-10 23:55:07 +0530

    Update Hello_World.c

--- C/Hello_World.c
+++ C/Hello_World.c
@@ -8,6 +8,5 @@ int main()
     char s[100];
     scanf("%[^\n]%*c", &s);
   	printf("Hello, World!\n%s",s);
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
