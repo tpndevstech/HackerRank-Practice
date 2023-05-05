6d33324f33eec0323c66f1c4f9761a711ae12627
Tapan Anand <70485285+tpndevstech@users.noreply.github.com>
2023-05-05 23:57:18 +0530

    Update Java-Static-Initializer-Block.java

--- Java/Java-Static-Initializer-Block.java
+++ Java/Java-Static-Initializer-Block.java
@@ -5,15 +5,24 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
+    static int B,H;
+    static boolean flag;
+    static{
+        Scanner Scn=new Scanner(System.in);
+        B=Scn.nextInt();
+        H=Scn.nextInt();
+        if(B>0&&H>0){
+            flag=true;
+        }else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scan = new Scanner(System.in);
-   int i = 0;
-        while(scan.hasNext()){
-       i++;
-       System.out.println(i + " " + scan.nextLine());
-   }
-   scan.close();
-    } 
-}
+}//end of class
