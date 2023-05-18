e145eb8b1dd4d959d3c3c510231959a53cecba0f
Tapan Anand <70485285+tpndevstech@users.noreply.github.com>
2023-05-18 22:48:27 +0530

    Create Java-Primality-Test.java

---
+++ Java/Java-Primality-Test.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      BigInteger n = in.nextBigInteger();
+      in.close();
+      System.out.println( n.isProbablePrime(1000) ? "prime" : "not prime" );
+   }
+}
